4e2b74d311b4eb10ff97c08f956b1f6e22172d6e
TCC <TCC>
2016-11-18 23:45:39 +0800

    Added door animation test; collider on level 0 objects

---
+++ Assets/Object/Scripts/Door/OpenDoor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OpenDoor : MonoBehaviour {
+	
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider collision){
+		//gameObject.GetComponent<Animation>().Play("open");
+		Animation doorAnim = gameObject.transform.parent.Find("door").GetComponent<Animation>();
+		if(doorAnim.isPlaying == false){
+			//Do reaction
+			doorAnim.Play("open");
+		}
+	}
+
+	void OnTriggerExit(Collider collision){
+		//gameObject.GetComponent<Animation>().Play("open");
+		/*
+		Animation doorAnim = gameObject.transform.parent.Find("door").GetComponent<Animation>();
+		if(doorAnim.isPlaying == false){
+			//Do reaction
+			doorAnim.Play("close");
+		}*/
+	}
+
+    void Update()
+    {
+		
+    }
+}
